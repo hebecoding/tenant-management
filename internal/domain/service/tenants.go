13522e7323a1c8cea7e068310869bcb86e99c8fc
HeBeCoding <dalvinjmatthews@gmail.com>
2023-06-06 22:34:10 -0400

    rearranged service methods in interface by alphabetical order

--- internal/domain/service/tenants.go
+++ internal/domain/service/tenants.go
@@ -2,20 +2,21 @@ package service
 
 import (
 	"context"
+
 	"github.com/hebecoding/tenant-management/internal/domain/entities"
 )
 
 type TenantService interface {
 	CreateTenant(ctx context.Context, tenant *entities.Tenant) (*entities.Tenant, error)
-	GetTenantByID(ctx context.Context, id string) (*entities.Tenant, error)
-	UpdateTenant(ctx context.Context, tenant *entities.Tenant) (*entities.Tenant, error)
 	DeleteTenant(ctx context.Context, id string) error
-	ListTenants(ctx context.Context, offset, limit int64) ([]*entities.Tenant, error)
+	GetTenantByID(ctx context.Context, id string) (*entities.Tenant, error)
+	GetTenantCompanies(ctx context.Context, id string) ([]*entities.TenantCompanyDetails, error)
+	GetTenantCompanyByID(ctx context.Context, id string, companyID string) (*entities.TenantCompanyDetails, error)
+	GetTenantPaymentDetails(ctx context.Context, id string) ([]*entities.TenantPaymentDetails, error)
 	GetTenantSubscription(ctx context.Context, id string) (*entities.TenantSubscriptionDetails, error)
-	UpdateTenantSubscription(ctx context.Context, id string, subscription *entities.TenantSubscriptionDetails) error
-	GetTenantCompany(ctx context.Context, id string) (*entities.TenantCompanyDetails, error)
+	ListTenants(ctx context.Context, offset, limit int64) ([]*entities.Tenant, error)
+	UpdateTenant(ctx context.Context, tenant *entities.Tenant) error
 	UpdateTenantCompany(ctx context.Context, id string, company *entities.TenantCompanyDetails) error
-	GetTenantPaymentDetails(ctx context.Context, id string) (*entities.TenantPaymentDetails, error)
+	UpdateTenantSubscription(ctx context.Context, id string, subscription *entities.TenantSubscriptionDetails) error
 	UpdateTenantPaymentDetails(ctx context.Context, id string, paymentDetails *entities.TenantPaymentDetails) error
-	UpdateTenantFields(ctx context.Context, id string, fieldsToUpdate map[string]interface{}) (*entities.Tenant, error)
 }
