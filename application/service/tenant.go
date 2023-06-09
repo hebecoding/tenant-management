515c06f22f8c5814ae783563950a0d8b46744d6a
HeBeCoding <dalvinjmatthews@gmail.com>
2023-06-08 23:04:38 -0400

    defined service methods

---
+++ application/service/tenant.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"context"
+
+	"github.com/hebecoding/tenant-management/internal/domain/entities"
+)
+
+type TenantService interface {
+	CreateTenant(ctx context.Context, tenant *entities.Tenant) (*entities.Tenant, error)
+	DeleteTenant(ctx context.Context, id string) error
+	GetTenantByID(ctx context.Context, id string) (*entities.Tenant, error)
+	GetTenantCompanies(ctx context.Context, id string) ([]*entities.TenantCompanyDetails, error)
+	GetTenantCompanyByID(ctx context.Context, id string, companyID string) (*entities.TenantCompanyDetails, error)
+	GetTenantPaymentDetails(ctx context.Context, id string) ([]*entities.TenantPaymentDetails, error)
+	GetTenantByPaymentID(ctx context.Context, paymentID string) (*entities.Tenant, error)
+	GetTenantSubscription(ctx context.Context, id string) (*entities.TenantSubscriptionDetails, error)
+	ListTenants(ctx context.Context) ([]*entities.Tenant, error)
+	UpdateTenant(ctx context.Context, tenant *entities.Tenant) error
+	UpdateTenantCompany(ctx context.Context, id string, company *entities.TenantCompanyDetails) error
+	UpdateTenantSubscription(ctx context.Context, id string, subscription *entities.TenantSubscriptionDetails) error
+	UpdateTenantPaymentDetails(ctx context.Context, id string, paymentDetails *entities.TenantPaymentDetails) error
+}
--- internal/domain/service/tenants.go
+++ internal/domain/service/tenants.go
@@ -1,23 +1,21 @@
 package service
 
 import (
-	"context"
-
-	"github.com/hebecoding/tenant-management/internal/domain/entities"
+	"github.com/hebecoding/digital-dash-commons/utils"
+	"github.com/hebecoding/tenant-management/internal/domain/repository"
 )
 
-type TenantService interface {
-	CreateTenant(ctx context.Context, tenant *entities.Tenant) (*entities.Tenant, error)
-	DeleteTenant(ctx context.Context, id string) error
-	GetTenantByID(ctx context.Context, id string) (*entities.Tenant, error)
-	GetTenantCompanies(ctx context.Context, id string) ([]*entities.TenantCompanyDetails, error)
-	GetTenantCompanyByID(ctx context.Context, id string, companyID string) (*entities.TenantCompanyDetails, error)
-	GetTenantPaymentDetails(ctx context.Context, id string) ([]*entities.TenantPaymentDetails, error)
-	GetTenantByPaymentID(ctx context.Context, paymentID string) (*entities.Tenant, error)
-	GetTenantSubscription(ctx context.Context, id string) (*entities.TenantSubscriptionDetails, error)
-	ListTenants(ctx context.Context, offset, limit int64) ([]*entities.Tenant, error)
-	UpdateTenant(ctx context.Context, tenant *entities.Tenant) error
-	UpdateTenantCompany(ctx context.Context, id string, company *entities.TenantCompanyDetails) error
-	UpdateTenantSubscription(ctx context.Context, id string, subscription *entities.TenantSubscriptionDetails) error
-	UpdateTenantPaymentDetails(ctx context.Context, id string, paymentDetails *entities.TenantPaymentDetails) error
+type TenantService struct {
+	Repository repository.TenantRepository
+	Logger     utils.LoggerInterface
+}
+
+func NewTenantService(
+	repository repository.TenantRepository,
+	logger utils.LoggerInterface,
+) *TenantService {
+	return &TenantService{
+		Repository: repository,
+		Logger:     logger,
+	}
 }
